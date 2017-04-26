b8878fa8ecde8043b9c1a5754ec3523f3ca3ea1e
Matt Maurer <maurer.it@gmail.com>
2017-04-25 21:12:38 -0400

    I think I'm ready to implement this?

--- gen_proto.sh
+++ gen_proto.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-protoc --go_out=plugins=micro:. --proto_path=$GOPATH/src --proto_path=proto warehouse.proto
+protoc --go_out=plugins=micro:. --proto_path=$GOPATH/src --proto_path=. warehouse.proto
